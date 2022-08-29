28ca37888f9ded1f8e5c14300536e5b0f7077795
suchi <suchibankoti95@gmail.com>
2022-08-29 07:54:40 +0530

    add a while loop to display books on the page, make i a global variable

--- library.js
+++ library.js
@@ -1,12 +1,12 @@
 let myLibrary = []
-
+let i = 0
 function addBookToLibrary() {
     const name = prompt("name of the book")
     const author = prompt("auhtor of the book")
     const publishYear = prompt("publish year of the book")
     const book = new bookDetail(name, author, publishYear)
     myLibrary.push(book)
-    console.log(myLibrary)
+
 }
 class bookDetail {
     constructor(name, author, publishYear) {
@@ -15,6 +15,18 @@ class bookDetail {
         this.publishYear = publishYear
     }
 }
+function bookDisplay() {
+    while (i < myLibrary.length) {
+        addBookToDisplay.innerHTML += `<ul><li> NAME OF THE BOOK :${myLibrary[i].name}</li>
+        <li> AUTHOR : ${myLibrary[i].author}</li>
+        <li> PUBLISH YEAR : ${myLibrary[i].publishYear}</li></ul>`
+        i++
+    }
+
+}
 
 const btnAddBook = document.getElementById("addBook")
 btnAddBook.addEventListener("click", addBookToLibrary)
+const addBookToDisplay = document.getElementById("bookDisplay")
+const viewBooks = document.getElementById("viewBook")
+viewBooks.addEventListener("click", bookDisplay)
