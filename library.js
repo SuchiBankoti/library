bf2e8019d8b58a042d42f09151d4865d902f63ee
suchi <suchibankoti95@gmail.com>
2022-08-30 22:41:28 +0530

    add condition operator to read/unread button to display its status

--- library.js
+++ library.js
@@ -1,51 +1,37 @@
 let myLibrary = []
 
-
 class bookDetail {
     constructor(name, author, publishYear,) {
         this.id = new Date().valueOf()
         this.name = name
         this.author = author
         this.publishYear = publishYear
-        // this.status = this.changeStatus
         this.read = false;
-        this.status = "unread"
     }
     toggleStatus = () => {
         this.read = !this.read;
+        console.log(this.read)
     }
 }
-const toggle = new bookDetail().toggleStatus
 const addBookToDisplay = document.getElementById("bookDisplay")
 
 function bookDisplay() {
     addBookToDisplay.innerHTML = " "
-    console.log("bookdispolay")
-
-    addBookToDisplay.innerHTML +=
-        myLibrary.map((value) => {
-            return `<ul class="book">
+    addBookToDisplay.innerHTML = myLibrary.map((value) => {
+        return `<ul class="book">
         <li> TITLE :${value.name}</li>
         <li> AUTHOR : ${value.author}</li>
         <li> PUBLISH YEAR : ${value.publishYear}</li>
-        <li>ID : ${value.id}</li>
-        <button>${value.status}</button>
         <button onClick='remove(${value.id})'>remove</button>
-        <button onClick='read(${value.id})'></button>
+        <button onClick='call(${value.toggleStatus})>'${(value.read) ? 'read' : 'unread'}</button>
         </ul>`
-        }).join("")
-
+    }).join("")
 }
-
-
-
-function status(value1, value2) {
-    value1(value2)
+function call(value) {
+    value()
 }
-
 
 function addBookToLibrary() {
-    // const id = prompt("ID")
     const name = prompt("name of the book")
     const author = prompt("auhtor of the book")
     const publishYear = prompt("publish year of the book")
@@ -54,7 +40,6 @@ function addBookToLibrary() {
     bookDisplay()
 }
 
-bookDisplay()
 
 function remove(value) {
     const index = myLibrary.findIndex((element) => element.id == value)
@@ -62,20 +47,8 @@ function remove(value) {
     bookDisplay()
 }
 
-function read(value) {
-    const index = myLibrary.findIndex((element) => element.id == value)
-    if (myLibrary[index].read == false) {
-        toggle()
-        myLibrary[index].status = "read"
-        bookDisplay()
-    }
-    // else {
-    //     toggle()
-    //     myLibrary[index].status = "unread"
-    //     bookDisplay()
-    // }
-    //   ee.target.style.backgroundColor="red"
-}
+bookDisplay()
+
 
 
 
--- testfile.js
+++ testfile.js
@@ -1,5 +1,31 @@
+const library = (() => {
 
-let arr = [34, 45, 666, 88]
-const newarr = arr.splice(1, 1)
-console.log(newarr)
-console.log(arr)
+    let collection = [];
+    function insert(book) {
+        collection.push(book);
+    }
+    function remove(bookId) {
+        const index = this.collection.findIndex(book => book.id === bookId)
+        this.collection.splice(index, 1);
+    }
+    return { insert, remove }
+})();
+library.insert(new)
+
+class book {
+    constructor(id, name) {
+        this.id = id
+        this.name = name
+    }
+    editAuthor(name) {
+        this.name = name;
+    }
+}
+
+const lib1 = new library();
+lib1.collection = null;
+lib1.insert(new book(1, 'jungle book'))
+lib1.insert(new book(2, 'jungle book'))
+console.log([...lib1.collection]);
+lib1.remove(1)
+// console.log(lib1)
